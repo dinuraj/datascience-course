f57703a60ce272f5f537f1c800da69809b6cfc5e
dinuraj <dinuraj@gmail.com>
2017-07-11 22:37:50 +0530

    ggplot assigment

--- LearningR/assigment_0710_q4.r
+++ LearningR/assigment_0710_q4.r
@@ -24,8 +24,8 @@ ggplot(data=freq_t1, aes(x=Drive, y=Frequency)) + geom_col()
 
 #d. Relationship between any two qualitative variables
 #Relationship between class and drive 
-#ggplot(data=mpg, aes(x=class, y=drv))+geom_point()
+ggplot(data=mpg, aes(x=class, y=drv))+geom_jitter()
 
 #e.     A quantitative variable and a qualitative variable . 
 #Average mileage of each class of vehicle. 
-
+ggplot(data=mpg, aes(x=class, y=hwy))+geom_jitter()
